8d22dd97eadc3b4c28f730273c526d46f226d416
Cory <ccrowley96@gmail.com>
2020-05-14 13:18:46 -0700

    added meme caching and post titles

--- reddit-bot/reddit-bot.js
+++ reddit-bot/reddit-bot.js
@@ -9,7 +9,9 @@ let memeSources = [{id: 'dankmemes', weight: 3},
                    {id: 'PrequelMemes', weight: 2}]
 
 let memeVault = {
-    unset: true
+    unset: true,
+    memeCache: [],
+    memeCount: 0
 }
 
 let timeDiff = {
@@ -25,8 +27,16 @@ const r = new snoowrap({
     refreshToken: process.env.REFRESH_TOKEN
 })
 
+//Testing
+// r.getSubreddit('memes')
+//         .getHot()
+//         .fetchMore({amount: 1, append: false})
+//         .then(posts => {
+//             console.log(posts.toJSON())
+//         }).catch(err => reject(err));
 
-module.exports = function({type, msg}){
+
+const redditBot = function({type, msg}){
     if(type == enums.randomMeme){
         sendRandomRedditMeme(msg).then(meme => {
             msg.channel.send(meme);
@@ -37,11 +47,17 @@ module.exports = function({type, msg}){
     }
 }
 
+module.exports = redditBot;
+
 async function updateMemes(msg){
     msg.channel.send('Sec, refreshing memes...');
+    memeVault.memeCache = [];
+    memeVault.memeCount = 0;
     //Fetch new memes from all sources
     for(let sub of memeSources){
-        memeVault[sub.id] = await setMemes({amount: 100, subreddit: sub.id});
+        let amount = 100;
+        memeVault.memeCount += amount;
+        memeVault[sub.id] = await setMemes({amount, subreddit: sub.id});
         //console.log(`Updated source ${sub.id}, ${memeVault[sub.id].length} new memes set`);
     }
 }
@@ -73,7 +89,18 @@ async function sendRandomRedditMeme(msg){
         return `${String(((weight / total) * 100))}%`
     }
 
-    return `r/${randomSource} selected with ${probString()} chance\n` + memeVault[randomSource][randomIdx];   
+    let selectedMeme = memeVault[randomSource][randomIdx];
+
+    //Check that meme hasn't been sent already
+    if(memeVault.memeCache.indexOf(selectedMeme.url) != -1 &&
+       memeVault.memeCache.length < ((.75) * memeVault.memeCount)){
+        // Meme already sent (retry)
+        return sendRandomRedditMeme(msg);
+    }
+
+    memeVault.memeCache.push(selectedMeme.url);
+
+    return `r/${randomSource} selected with ${probString()} chance\n ${selectedMeme.title} : (${selectedMeme.ups} upvotes)\n ${selectedMeme.url}`;   
 }
 
 async function setMemes({amount, subreddit}){
@@ -82,7 +109,12 @@ async function setMemes({amount, subreddit}){
         .getHot()
         .fetchMore({amount, append: false})
         .filter(post => post.url)
-        .map(post => post.url).then(memez => {
+        .map(post => {
+            return {url: post.url, 
+                    title: post.title,
+                    ups: String(post.ups)}
+        })
+        .then(memez => {
             timeDiff.lastUpdate = Date.now();
             resolve(memez);
         }).catch(err => reject(err));
